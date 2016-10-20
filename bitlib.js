7635c2d13554f2ed0d486a8e0531ecf24be9e415
Keith Peters <kp@bit-101.com>
2016-10-20 06:54:07 -0400

    extendCanvas

--- bitlib.js
+++ bitlib.js
@@ -111,17 +111,47 @@ bitlib.math = {
 bitlib.context = function (w, h, parent) {
     var canvas = document.createElement("canvas");
     canvas.style.display = "block";
-    canvas.width = w;
-    canvas.height = h;
-    canvas.getContext("2d");
-    if (!parent) {
-        parent = document.body;
-    }
+    canvas.width = w || 600;
+    canvas.height = h || 600;
+    parent = parent || document.body;
     parent.appendChild(canvas);
-    return canvas.getContext("2d");
+    var context = canvas.getContext("2d");
+    bitlib.extendContext(context);
+    return context;
 };
 
+bitlib.extendContext = function(context) {
 
+    context.setShadow =function(color, offsetX, offsetY, blur) {
+        this.shadowColor = color;
+        this.shadowOffsetX = offsetX;
+        this.shadowOffsetY = offsetY;
+        this.shadowBlur = blur;
+    };
+
+    context.clear = function() {
+        this.save();
+        this.setTransform(1, 0, 0, 1, 0, 0);
+        this.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.restore();
+    }
+
+    context.circle = function(x, y, radius) {
+        context.arc(x, y, radius, 0, Math.PI * 2);
+    }
+
+    context.fillCircle = function(x, y, radius) {
+        context.beginPath();
+        context.circle(x, y, radius);
+        context.fill();
+    }
+
+    context.strokeCircle = function(x, y, radius) {
+        context.beginPath();
+        context.circle(x, y, radius);
+        context.stroke();
+    }
+};
 
 bitlib.color = {
     rgb: function(r, g, b) {
